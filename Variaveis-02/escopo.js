044a945e3675cb2a5cb4feeff76652295c908a76
Lucas Massoni <lucas.masson0307@gmail.com>
2025-03-19 16:30:04 -0300

    19/03

---
+++ Variaveis-02/escopo.js
@@ -0,0 +1,17 @@
+/*
+if (1 > 0) {
+    var estudante = 'Caroline';
+    console.log(estudante);
+}
+
+var estudante = 'Ana';//quando atribuido um novo valor fora do bloco seu valor muda
+console.log(estudante);
+*/
+let estudante;
+
+if (1 > 0) {
+    estudante = 'Caroline';
+    console.log(estudante);
+}
+
+console.log(estudante);
---
+++ Variaveis-02/variaveis.js
@@ -0,0 +1,4 @@
+const estudante = 'Caroline'; //const - nao pode ser reatribuida
+let professora = 'Ana';//let ou var - podem ser reatribuidas
+
+console.log(estudante, professora); //console.log = mostrar info
