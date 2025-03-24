b16b67f873b2f15be2c3fa1b74686ef71e5c5b15
Lucas Massoni <lucas.masson0307@gmail.com>
2025-03-24 15:19:57 -0300

    24/03

---
+++ Dados-03/tipo-number.js
@@ -0,0 +1,8 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBI = Number.parseInt('5'); // "Number.parseint() reconhece o string como numero"
+
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBI) / 4;
+
+console.log(' a media e ' + total.toFixed(2));
---
+++ Variaveis-02/exercicios.js
@@ -0,0 +1,41 @@
+/*
+#1
+// Declarando as variáveis
+let nome = "João";  // string
+let idade = 25;     // número
+let isEstudante = true; // booleano
+
+// Exibindo o tipo de cada variável no console
+console.log(typeof nome);       // tipo da variável "nome"
+console.log(typeof idade);      // tipo da variável "idade"
+console.log(typeof isEstudante); // tipo da variável "isEstudante"
+
+#2
+let nome = "lucas";
+let nome2 = "massoni";
+let nomes = nome + " " + nome2;
+console.log(nomes)
+
+#3
+let nome = "lucas";
+let idade = 18;
+let dados = nome + " " + idade;
+console.log(dados)
+
+#4
+//var fora e dentro do bloco
+var nome = "lucas";
+if (1>0){
+    var nome ="joao";
+    console.log(nome)
+}
+console.log(nome)
+
+//let fora e dentro do bloco
+let nome = "lucas";
+if (1>0){
+    let nome = "joao";
+    console.log(nome)
+}
+console.log(nome)
+*/
